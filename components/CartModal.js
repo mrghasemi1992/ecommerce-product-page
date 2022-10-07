93b7ea06217d08a1d411394adac5088ccda8df39
Mohammad Reza Ghasemi <mrghasemi1992@gmail.com>
2022-10-07 11:43:04 +0330

    [feat] animation

--- components/CartModal.js
+++ components/CartModal.js
@@ -1,57 +1,58 @@
 import Image from "next/image";
-import { useRef } from "react";
-import { useClickAway } from "react-use";
+import { motion } from "framer-motion";
 
 const CartModal = ({ cartNumber, setCartModalIsOpen, setCartNumber }) => {
-  const cartModalRef = useRef(null);
-
-  useClickAway(cartModalRef, () => {
-    setCartModalIsOpen(false);
-  });
-
   return (
-    <div
-      ref={cartModalRef}
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.3, ease: "easeInOut" }}
       className="bg-white absolute rounded-lg z-10 top-[70px] w-[calc(100%-1rem)] left-1/2 transform -translate-x-1/2"
     >
       <h1 className="p-4">Cart</h1>
       <hr />
       {cartNumber ? (
-        <div className="p-4 flex items-center justify-between">
-          <div className="flex items-center gap-x-4">
-            <Image
-              src="/assets/images/image-product-1-thumbnail.jpg"
-              alt="Product"
-              className="rounded-lg"
-              width={50}
-              height={50}
-            />
-            <div>
-              <p className="text-neutral-darkGrayishBlue font-light">
-                Autumn Limited Edition...
-              </p>
-              <p className="text-neutral-darkGrayishBlue font-light">
-                $125 × {cartNumber}{" "}
-                <span className="font-semibold text-neutral-darkBlue">
-                  ${cartNumber * 125}
-                </span>
-              </p>
+        <div className="p-4">
+          <div className="flex items-center justify-between">
+            <div className="flex items-center gap-x-4">
+              <Image
+                src="/assets/images/image-product-1-thumbnail.jpg"
+                alt="Product"
+                className="rounded-lg"
+                width={50}
+                height={50}
+              />
+              <div>
+                <p className="text-neutral-darkGrayishBlue font-light">
+                  Autumn Limited Edition...
+                </p>
+                <p className="text-neutral-darkGrayishBlue font-light">
+                  $125 × {cartNumber}{" "}
+                  <span className="font-semibold text-neutral-darkBlue">
+                    ${cartNumber * 125}
+                  </span>
+                </p>
+              </div>
             </div>
+            <Image
+              src="/assets/icons/icon-delete.svg"
+              width={14}
+              height={16}
+              alt="Trash"
+              onClick={() => setCartNumber(0)}
+            />
           </div>
-          <Image
-            src="/assets/icons/icon-delete.svg"
-            width={14}
-            height={16}
-            alt="Trash"
-            onClick={() => setCartNumber(0)}
-          />
+          <button className="flex items-center justify-center bg-primary-orange h-14 w-full rounded-lg text-white mt-4">
+            Checkout
+          </button>
         </div>
       ) : (
         <p className="text-neutral-darkGrayishBlue min-h-[140px] flex items-center justify-center">
           Your cart is empty.
         </p>
       )}
-    </div>
+    </motion.div>
   );
 };
 
--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,4 @@
+import { AnimatePresence, motion } from "framer-motion";
 import Image from "next/image";
 import { useRef, useState } from "react";
 import { useClickAway } from "react-use";
@@ -9,8 +10,10 @@ const Header = ({ cartNumber, setCartNumber }) => {
   const [menuIsOpen, setMenuIsOpen] = useState(false);
 
   const menuRef = useRef(null);
+  const cartModalRef = useRef(null);
 
   useClickAway(menuRef, () => setMenuIsOpen(false));
+  useClickAway(cartModalRef, () => setCartModalIsOpen(false));
 
   return (
     <div className="bg-white p-4 flex justify-between items-center relative">
@@ -31,21 +34,33 @@ const Header = ({ cartNumber, setCartNumber }) => {
       </div>
 
       <div className="flex items-center gap-x-4">
-        <div className="relative flex">
-          <Image
-            src="/assets/icons/icon-cart.svg"
-            width={22}
-            height={20}
-            alt="Cart"
-            onClick={() => setCartModalIsOpen(!cartModalIsOpen)}
-          />
-          {cartNumber ? (
-            <div className="absolute bg-primary-orange text-white px-2 text-xs rounded-xl -top-2 -right-2">
-              {cartNumber}
-            </div>
-          ) : (
-            <></>
-          )}
+        <div ref={cartModalRef}>
+          <AnimatePresence>
+            {cartModalIsOpen && (
+              <CartModal
+                cartNumber={cartNumber}
+                setCartModalIsOpen={setCartModalIsOpen}
+                setCartNumber={setCartNumber}
+              />
+            )}
+          </AnimatePresence>
+          <div className="relative flex">
+            <Image
+              src="/assets/icons/icon-cart.svg"
+              width={22}
+              height={20}
+              alt="Cart"
+              onClick={() => setCartModalIsOpen(!cartModalIsOpen)}
+            />
+
+            {cartNumber ? (
+              <div className="absolute bg-primary-orange text-white px-2 text-xs rounded-xl -top-2 -right-2">
+                {cartNumber}
+              </div>
+            ) : (
+              <></>
+            )}
+          </div>
         </div>
         <Image
           src="/assets/images/image-avatar.png"
@@ -55,36 +70,43 @@ const Header = ({ cartNumber, setCartNumber }) => {
         />
       </div>
 
-      {cartModalIsOpen && (
-        <CartModal
-          cartNumber={cartNumber}
-          setCartModalIsOpen={setCartModalIsOpen}
-          setCartNumber={setCartNumber}
-        />
-      )}
-
-      {menuIsOpen && (
-        <div className="bg-black bg-opacity-40 fixed inset-0 z-10">
-          <div className="bg-white w-2/3 h-full p-6" ref={menuRef}>
-            <Image
-              src="/assets/icons/icon-close.svg"
-              width={14}
-              height={15}
-              alt="Close"
-              onClick={() => setMenuIsOpen(false)}
-            />
-            <div className="flex gap-y-6 flex-col mt-6">
-              <p className="text-dark-blue font-semibold text-xl">
-                Collections
-              </p>
-              <p className="text-dark-blue font-semibold text-xl">Men</p>
-              <p className="text-dark-blue font-semibold text-xl">Women</p>
-              <p className="text-dark-blue font-semibold text-xl">About</p>
-              <p className="text-dark-blue font-semibold text-xl">Contact</p>
-            </div>
-          </div>
-        </div>
-      )}
+      <AnimatePresence>
+        {menuIsOpen && (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.3, ease: "easeInOut" }}
+            className="bg-black bg-opacity-40 fixed inset-0 z-10"
+          >
+            <motion.div
+              initial={{ x: -300 }}
+              animate={{ x: 0 }}
+              exit={{ x: -300 }}
+              transition={{ duration: 0.3, ease: "easeInOut" }}
+              className="bg-white w-2/3 h-full p-6"
+              ref={menuRef}
+            >
+              <Image
+                src="/assets/icons/icon-close.svg"
+                width={14}
+                height={15}
+                alt="Close"
+                onClick={() => setMenuIsOpen(false)}
+              />
+              <div className="flex gap-y-6 flex-col mt-6">
+                <p className="text-dark-blue font-semibold text-xl">
+                  Collections
+                </p>
+                <p className="text-dark-blue font-semibold text-xl">Men</p>
+                <p className="text-dark-blue font-semibold text-xl">Women</p>
+                <p className="text-dark-blue font-semibold text-xl">About</p>
+                <p className="text-dark-blue font-semibold text-xl">Contact</p>
+              </div>
+            </motion.div>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 };
